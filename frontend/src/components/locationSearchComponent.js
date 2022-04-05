b7215be0a6e8baa6c8fe60e0816e38d6087481ac
Jenral <dennis_otu@live.com>
2022-04-05 11:09:55 +0000

    ++pigeon maps

--- frontend/src/components/locationSearchComponent.js
+++ frontend/src/components/locationSearchComponent.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
+import { Map, Marker } from 'pigeon-maps'
 
 const LocationSearch = () => {
 	const [location, setLocation] = useState('')
 	const [place, setPlace] = useState('')
 
 	const [cribs, setCribs] = useState(null)
-	const [page, setPage] = useState(0)
+  const [page, setPage] = useState(0)
+  
+
     let currentPage = page+1
 
     useEffect(()=>{
@@ -39,18 +42,28 @@ const LocationSearch = () => {
           <input value={place} className="form-field mx-2" placeholder="enter location" onChange={handleInputChange}
             name="location"/>
         </div>
+        
         <button onClick={getLocationCribs} className="btn btn-success">
           Submit
         </button>
+
         {cribs && <div> {`page ${currentPage}`} <button className='mx-2' onClick={nextPage}> next page </button> </div>}
-    	{currentPage > 1 && <button onClick={previousPage}>previous page</button>}
-    	<ul>
+    	  {currentPage > 1 && <button onClick={previousPage}>previous page</button>}
+    	  
+        <ul>
         {cribs && cribs.map(crib => <li>{crib.name}, {crib.address.country}</li>)}
         </ul> 
+        
+        {cribs &&
+          <Map height={400} width={800} center={ [ cribs[0].address.location.coordinates[1], cribs[0].address.location.coordinates[0] ] } defaultZoom={11} animate={true}>
+            {cribs && cribs.map(crib => <Marker width={50}
+              anchor={[crib.address.location.coordinates[1], crib.address.location.coordinates[0]]} />)
+            }
+          </Map>
+        }      
       </div> 
 
     )
 }
 
 export default LocationSearch
-
